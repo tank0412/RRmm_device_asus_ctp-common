0e8c9dc1c6072e9f67fa030848391762e6cdc837
Benoît Laniel <nels+git@pgroupe.net>
2015-12-24 10:12:47 +0100

    t00f: add opensource sensors (based on akm libsensors)

--- ctp.mk
+++ ctp.mk
@@ -237,6 +237,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	libstlport
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.redhookbay
+
 # Features removed from "user" builds
 PRODUCT_PACKAGES += \
 	su \
