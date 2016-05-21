73b80330656e4f4570b457d2f5b33e751f9a33a4
tank0412 <romabukhtiarov@gmail.com>
2016-05-21 19:16:46 +0300

    Revert "ctp-common: switch to latest ramdisk by @dgadelha"
    This reverts commit dea06eb5ffb9c045cc84b6ce8b37852ca833ff43.

--- ramdisk/sbin/dnfix.sh
+++ ramdisk/sbin/dnfix.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 
-# Device Name Fix for ASUS Zenfone 5/6
+# Device Name Fix for ASUS Zenfone 5/6 Sensors
 # Author: Douglas Gadêlha <douglas@gadeco.com.br>
-# Version: 20160517
+# Version: 20160501
 #
 # The MIT License (MIT)
 #
@@ -26,30 +26,16 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# NOTE: This feature is ENABLED by DEFAULT.
-
-if [ -f "/data/dgadelha/dnfix" ]; then
-	ENABLED=`cat "/data/dgadelha/dnfix"`
-
-	if [ "$ENABLED" == "0" ]; then
-		log -p i -t dnfix "Device Name Fix is DISABLED"
-		log -p i -t dnfix "To enable Device Name Fix please run the following command:"
-		log -p i -t dnfix "echo \"1\" > /data/dgadelha/dnfix"
-		log -p i -t dnfix "Exiting"
-		exit
-	fi
-fi
-
 PROJECT_ID=$(cat /sys/module/intel_mid_sfi/parameters/project_id)
 
 if [ -n "$PROJECT_ID" ]; then
 	if [ "$PROJECT_ID" -ge 0 -a "$PROJECT_ID" -le 4 ]; then
-		log -p i -t dnfix "Detected Device: ASUS Zenfone 5"
+		log -p i -t fixdevicename "Detected Device: ASUS Zenfone 5"
 		setprop ro.product.device ASUS_T00F
 		stop akmd_a600cg
 		start akmd
 	elif [ "$PROJECT_ID" -eq 5 -o "$PROJECT_ID" -eq 7 ]; then
-		log -p i -t dnfix "Detected Device: ASUS Zenfone 6"
+		log -p i -t fixdevicename "Detected Device: ASUS Zenfone 6"
 		setprop ro.product.device ASUS_T00G
 		stop akmd
 		start akmd_a600cg
@@ -57,34 +43,17 @@ if [ -n "$PROJECT_ID" ]; then
 		UPDATED=$(getprop dgadelha.dnfix.updated))
 
 		if [ "$UPDATED" == "true" ]; then
-			log -p i -t dnfix "Build Properties already updated"
+			log -p i -t fixdevicename "Build Properties already updated"
 		else
-			log -p i -t dnfix "Build Properties are NOT updated, updating..."
-
-			log -p i -t dnfix "Re-mounting System as Read-Write..."
+			log -p i -t fixdevicename "Build Properties are NOT updated, updating..."
+			log -p i -t fixdevicename "Re-mounting System as Read-Write..."
 			mount -o remount,rw /system
-
-			log -p i -t dnfix "Updating build.prop..."
+			log -p i -t fixdevicename "Updating build.prop..."
 			sed -i 's/T00F/T00G/g' /system/build.prop
-			echo "\n# dnfix\ndgadelha.dnfix.updated=true\n" >> /system/build.prop
-
-			log -p i -t dnfix "Updating Sensors Library..."
-			rm /system/lib/hw/sensors.redhookbay.so
-			ln -s /system/lib/hw/a600cg.sensors.redhookbay.so /system/lib/hw/sensors.redhookbay.so
-
-			log -p i -t dnfix "Re-inforcing permissions of modified files..."
-			chmod 0644 /system/lib/hw/a500cg.sensors.redhookbay.so
-			chmod 0644 /system/lib/hw/a600cg.sensors.redhookbay.so
-			chmod 0644 /system/lib/hw/sensors.redhookbay.so
-			chmod 0644 /system/build.prop
-
-			log -p i -t dnfix "Re-mounting System as Read-Only..."
+			echo "dgadelha.dnfix.updated=true" >> /system/build.prop
+			log -p i -t fixdevicename "Re-mounting System as Read-Only..."
 			mount -o remount,ro /system
-
-			log -p i -t dnfix "Build Properties updated. You may need to reboot to apply the changes"
+			log -p i -t fixdevicename "Build Properties updated. You may need to reboot to apply the changes"
 		fi
-	else
-		log -p i -t dnfix "Unknown device. Exiting"
-		exit
 	fi
 fi
